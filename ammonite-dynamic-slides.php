15dd6326a48c01b166be0c9a3db678808967e078
Daniel Ellis <danielshawellis@gmail.com>
2020-03-25 18:44:23 -0500

    add plugin headers

---
+++ ammonite-dynamic-slides.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name:       Ammonite Dynamic Slides
+ * Description:       Dynamic, treeing, AJAX-enabled slides built with a custom post type
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
