df9958ba5b47a4d892f2127e5d75e3d80d765663
Daniel Ellis <danielshawellis@gmail.com>
2020-03-29 14:45:48 -0500

    add slide transition animation handling

--- assets/js/ammonite-dynamic-slides.js
+++ assets/js/ammonite-dynamic-slides.js
@@ -5,7 +5,8 @@ ammoniteDynamicSlides = function() {
 
   // SCRIPT SETTINGS
   let settings = {
-
+    opacityTransitionDuration: 1500,
+    heightTransitionDuration: 1500
   }
 
   // ASSOCIATED CLASSES
@@ -28,40 +29,92 @@ ammoniteDynamicSlides = function() {
 
   // SCRIPT METHODS
   function loadNewSlide( slideId ) {
-    let newSlideUrl = localizedData.ajaxUrl + slideId;
+    // INITIAL AJAX
+      // Get slide and call success or failure
+      let newSlideUrl = localizedData.ajaxUrl + slideId;
+      jQuery.ajax( {
+        type: 'get',
+        dataType: 'json',
+        url: newSlideUrl,
+        success: function( response ) {
+          handleAjaxSuccess( response );
+        }
+      } );
 
-    // Get slide
-    jQuery.ajax( {
-      type: 'get',
-      dataType: 'json',
-      url: newSlideUrl,
-      success: function( response ) {
-        handleAjaxSuccess( response );
+    // AJAX HANDLERS
+      // Handle AJAX success
+      function handleAjaxSuccess( response ) {
+        // Set inner container's HTML
+        jQuery( '.' + classes.innerContainer ).html( response );
+
+        // Bring in new slide
+        bringInNewSlide();
       }
-    } );
 
-    // Handle AJAX success
-    function handleAjaxSuccess( response ) {
-      console.log( response );
-    }
+      // Handle AJAX failure
+      function handleAjaxFailure() {
 
-    // Handle AJAX failure
-    function handleAjaxFailure() {
+      }
 
-    }
+    // ANIMATION MAIN FUNTIONS
+      // Bring in loading animation immediately (this function is invoked immediately)
+      (function bringInLoadingAnimation() {
+        jQuery( '.' + classes.outerContainer ).queue( fadeOutSlide );
+        jQuery( '.' + classes.outerContainer ).queue( fadeInLoadingAnimation );
+      })();
+
+      // Bring in a new slide
+      function bringInNewSlide() {
+        console.log( 'bringInNewSlide ran' );
+        resizeOuterContainer();
+        jQuery( '.' + classes.outerContainer ).queue( fadeOutLoadingAnimation );
+        jQuery( '.' + classes.outerContainer ).queue( fadeInSlide );
+      }
+
+    // ANIMATION HELPER FUNCTIONS
+      // Fade out slide
+      function fadeOutSlide( next ) {
+        jQuery( '.' + classes.innerContainer ).animate( {
+          opacity: 0
+        }, settings.opacityTransitionDuration / 2, next );
+      }
+
+      // Fade in loading animation
+      function fadeInLoadingAnimation( next ) {
+        jQuery( '.' + classes.loadingContainer ).animate( {
+          opacity: 1
+        }, settings.opacityTransitionDuration / 2, next );
+      }
+
+      // Resize outer container
+      function resizeOuterContainer( next ) {
+        // Get slide height
+        let innerContainerHeight = jQuery( '.' + classes.innerContainer ).outerHeight();
+
+        // Animate outer container height
+        jQuery( '.' + classes.outerContainer ).animate( {
+          'height': innerContainerHeight + 'px'
+        }, settings.heightTransitionDuration, next );
+
+        // Animate loading animation container height alongside outer container
+        jQuery( '.' + classes.loadingContainer ).animate( {
+          'height': innerContainerHeight + 'px'
+        }, settings.heightTransitionDuration );
+      }
+
+      // Fade out loading animation
+      function fadeOutLoadingAnimation( next ) {
+        jQuery( '.' + classes.loadingContainer ).animate( {
+          opacity: 0
+        }, settings.opacityTransitionDuration / 2, next );
+      }
+
+      // Fade in slide
+      function fadeInSlide( next ) {
+        jQuery( '.' + classes.innerContainer ).animate( {
+          opacity: 1
+        }, settings.opacityTransitionDuration / 2, next );
+      }
   }
+  
 }();
-
-
-// jQuery( document ).ready( function() {
-//   jQuery( 'button' ).click( function() {
-//     jQuery.ajax( {
-//       type: 'get',
-//       dataType: 'json',
-//       url: 'http://localhost/creatinghealth/wp-json/ammonite-dynamic-slides/v1/slides/2104',
-//       success: function( response ) {
-//         jQuery( '.ammonite-dynimic-slides-inner-container' ).html( response );
-//       }
-//     } );
-//   } );
-// } );
--- templates/dynamic-slides-container.php
+++ templates/dynamic-slides-container.php
@@ -1,5 +1,5 @@
 <!-- <button type="button" name="button">Test</button> -->
-<div class="ammonite-dynamic-slides-outer-container">
+<div class="ammonite-dynamic-slides-outer-container" style="">
   <!-- Animation from SpinKit: https://github.com/tobiasahlin/SpinKit -->
   <div class="ammonite-dynamic-slides-loading-spinner-container">
     <div class="ammonite-dynamic-slides-loading-spinner">
@@ -12,7 +12,5 @@
     </div>
   </div>
   <div class="ammonite-dynimic-slides-inner-container">
-    <h1>This is a test</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </div>
