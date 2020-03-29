e59d748ff2905f987395217594d8e5e64d0ee213
Daniel Ellis <danielshawellis@gmail.com>
2020-03-29 10:54:15 -0500

    add module pattern and scaffold for script

--- assets/js/ammonite-dynamic-slides.js
+++ assets/js/ammonite-dynamic-slides.js
@@ -1,12 +1,25 @@
-jQuery( document ).ready( function() {
-  jQuery( 'button' ).click( function() {
-    jQuery.ajax( {
-      type: 'get',
-      dataType: 'json',
-      url: 'http://localhost/creatinghealth/wp-json/ammonite-dynamic-slides/v1/slides/2104',
-      success: function( response ) {
-        jQuery( '.ammonite-dynimic-slides-inner-container' ).html( response );
-      }
-    } );
-  } );
-} );
+// Create a single IEEF-based global module for this script
+ammoniteDynamicSlides = function() {
+  // SCRIPT SETTINGS
+
+  // ASSOCIATED CLASSES
+
+  // EVENT LISTENERS
+
+  // SCRIPT METHODS
+
+}();
+
+
+// jQuery( document ).ready( function() {
+//   jQuery( 'button' ).click( function() {
+//     jQuery.ajax( {
+//       type: 'get',
+//       dataType: 'json',
+//       url: 'http://localhost/creatinghealth/wp-json/ammonite-dynamic-slides/v1/slides/2104',
+//       success: function( response ) {
+//         jQuery( '.ammonite-dynimic-slides-inner-container' ).html( response );
+//       }
+//     } );
+//   } );
+// } );
