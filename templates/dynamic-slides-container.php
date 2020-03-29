f6328f726b1c7b632903e0fca14ce7fee3802666
Daniel Ellis <danielshawellis@gmail.com>
2020-03-29 11:54:04 -0500

    create basic slide styling and add loading animation

--- templates/dynamic-slides-container.php
+++ templates/dynamic-slides-container.php
@@ -1,6 +1,18 @@
-<button type="button" name="button">Test</button>
+<!-- <button type="button" name="button">Test</button> -->
 <div class="ammonite-dynamic-slides-outer-container">
+  <!-- Animation from SpinKit: https://github.com/tobiasahlin/SpinKit -->
+  <div class="ammonite-dynamic-slides-loading-spinner-container">
+    <div class="ammonite-dynamic-slides-loading-spinner">
+      <div class="ammonite-dynamic-slides-loading-spinner-dot"></div>
+      <div class="ammonite-dynamic-slides-loading-spinner-dot"></div>
+      <div class="ammonite-dynamic-slides-loading-spinner-dot"></div>
+      <div class="ammonite-dynamic-slides-loading-spinner-dot"></div>
+      <div class="ammonite-dynamic-slides-loading-spinner-dot"></div>
+      <div class="ammonite-dynamic-slides-loading-spinner-dot"></div>
+    </div>
+  </div>
   <div class="ammonite-dynimic-slides-inner-container">
-
+    <h1>This is a test</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </div>
